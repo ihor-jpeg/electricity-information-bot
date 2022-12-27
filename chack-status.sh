edd6d12effc524b698ba086674b95e903433eb69
Ihor <ihor@civilization.local>
2022-12-28 00:36:40 +0200

    minor fixes

--- chack-status.sh
+++ chack-status.sh
@@ -8,9 +8,12 @@ GPIO=18
 
 echo -e "Watching the power source...\n"
 
+if [ !-d /sys/class/gpio/gpio${GPIO}/direction ]; then
+        mkdir -p /sys/class/gpio/gpio${GPIO} && touch direction
+fi
+
 echo "in" > /sys/class/gpio/gpio"${GPIO}"/direction
 
-# create export folder if does not exist
 if [ ! -d /sys/class/gpio/gpio${GPIO} ]; then
         echo "${GPIO}" > /sys/class/gpio/export
 fi
